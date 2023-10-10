46b4b507425e4d4fcbd35ff8a47278f0062ff81e
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-10-11 01:51:34 +0300

    Lab_4.6

---
+++ Lab_4.6.cpp
@@ -0,0 +1,70 @@
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main() {
+	double P, S;
+	int k, i;
+	P = 0;
+	i = 1;
+	while (i <= 15)
+
+	{
+		S = 1;
+		k = 1;
+		while (k <= i)
+
+		{
+			S *= k * k + 1;
+			k++;
+
+		}
+		P += S / (1 + S * S);
+		i++;
+
+	}
+	cout << P << endl;
+	P = 0;
+	i = 1;
+	do
+	{
+		S = 1;
+		k = 1;
+		do
+		{
+			S *= k * k + 1;
+			k++;
+		} while (k <= i);
+		P += S / (1 + S * S);
+		i++;
+	} while (i <= 15);
+	cout << P << endl;
+	P = 0;
+	for (i = 1; i <= 15; i++)
+
+	{
+		S = 1;
+		for (k = 1; k <= i; k++)
+
+		{
+			S *= k * k + 1;
+
+		}
+		P += S / (1 + S * S);
+
+	}
+	cout << P << endl;
+	P = 0;
+	for (i = 15; i >= 1; i--)
+
+	{
+		S = 1;
+		for (k = i; k >= 1; k--)
+
+		{
+			S *= k * k + 1;
+		}
+		P += S / (1 + S * S);
+	}
+	cout << P << endl;
+	return 0;
+}
